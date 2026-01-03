186af840b3b546e85a6a3e554f4bb578a4b962e9
Ian King <148726980+imk24@users.noreply.github.com>
2026-01-03 18:56:27 -0500

    Refactor R_print.R to improve data handling
    Updated the script to make a copy of the data frame and clean up NA values.

--- R_print.R
+++ R_print.R
@@ -1,17 +1,31 @@
+#Start off by making a copy of the data frame
+df <- R_out
 View(R_out)
 
+
 #View All Headers
-names(R_out)
+names(df)
 
 #View all subjects, grades, and associated teachers
-x1 <- R_out[1:3]
+x1 <- df[1:3]
 
 View(x1)
 
 #Replaces all blank teachers with entry 'Not Found'
 x1[["Teacher.s."]][x1[["Teacher.s."]] == ""] <- "Not Found"
-R_out[1:3] <- x1
 
-View(R_out)
+df[1:3] <- x1
+
+r_count <- nrow(df)
+
+#Now getting rid of all of the na's to clean up the chart
+x1 <- df[4:r_count]
+
+x1[is.na(x1)] <- ""
+
+df[4:r_count] <- x1
+
+#Final Product
+View(df)
                               
 
