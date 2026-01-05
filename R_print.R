aa18873c441904a2a6daa2bbe1cb349784524247
Ian King <148726980+imk24@users.noreply.github.com>
2026-01-04 23:59:04 -0500

    Refactor R_print.R for clarity and consistency

--- R_print.R
+++ R_print.R
@@ -14,6 +14,7 @@ x1[["Teacher.s."]][x1[["Teacher.s."]] == ""] <- "Not Found"
 
 df[1:3] <- x1
 
+#Subtract by two to not include start and end time col headers.
 x = ncol(df) - 2
 #Now getting rid of all of the na's to clean up the chart
 x1 <- df[3:x]
@@ -24,3 +25,4 @@ df[3:x] <- x1
 
 #Final Product
 View(df)
+
