11635dda8987ed24ad2ef769f0ac215be67307c2
Ian King <148726980+imk24@users.noreply.github.com>
2026-01-04 23:38:08 -0500

    Refactor R_print.R for better NA handling
    Updated the range for cleaning NA values and improved code structure.

--- R_print.R
+++ R_print.R
@@ -1,7 +1,6 @@
 #Using R To sharpen output
 df <- R_out
 
-
 #View All Headers
 names(df)
 
@@ -14,15 +13,14 @@ View(x1)
 x1[["Teacher.s."]][x1[["Teacher.s."]] == ""] <- "Not Found"
 
 df[1:3] <- x1
-r_count <- nrow(df)
 
+x = ncol(df) - 2
 #Now getting rid of all of the na's to clean up the chart
-x1 <- df[3:r_count+1]
+x1 <- df[3:x]
 
 x1[is.na(x1)] <- ""
 
-df[3:r_count+1] <- x1
+df[3:x] <- x1
 
 #Final Product
 View(df)
-                              
